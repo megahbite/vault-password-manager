68e3f6183d984057293f96fab54a38d57013a1dc
Megan Bowra-Dean <megan@rabidtech.co.nz>
2016-01-12 22:15:05 +1300

    Add ember-simple-auth, stash root token.

--- app/controllers/initialize.js
+++ app/controllers/initialize.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  session: Ember.inject.service(),
   actions: {
     didInitialize(keys) {
       this.set('keys', keys);
@@ -8,6 +9,8 @@ export default Ember.Controller.extend({
       Ember.$('body').addClass('modal-open');
     },
     keysViewed() {
+      this.get('session').set('token', this.get('keys').root_token);
+      this.get('session').set('isRoot', true);
       this.transitionToRoute('unseal');
     }
   }
